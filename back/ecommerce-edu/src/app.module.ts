8e90b9dbf9920afcf2fd10df2a721c65eb35961b
Eduardo Ruiz <ruizzmarin20@gmail.com>
2024-06-21 16:54:10 -0300

    creacion de modulos, controladores y servicios

--- back/ecommerce-edu/src/app.module.ts
+++ back/ecommerce-edu/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsController } from './products/products.controller';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UsersModule, AuthModule, ProductsModule],
+  controllers: [AppController, ProductsController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ back/ecommerce-edu/src/middlewares/logger.middleware.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+export function LoggerMiddleware(req: Request, res: Response, next: NextFunction) {
+    const actualDate = new Date();
+    const date = actualDate.toLocaleDateString();
+    const time = actualDate.toLocaleTimeString();
+    console.log(`${req.method} ${req.url} - ${date} ${time}`);
+    next();
+}
