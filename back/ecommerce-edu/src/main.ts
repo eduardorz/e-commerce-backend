dab7c6347b681c3037d929da0d4bc1ead2ed7c57
Eduardo Ruiz <ruizzmarin20@gmail.com>
2024-07-25 22:05:26 -0300

    api swagger

--- back/ecommerce-edu/src/main.ts
+++ back/ecommerce-edu/src/main.ts
@@ -2,9 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Proyecto Integrador M4-Back Eduardo Ruiz')
+    .setDescription('Aplicacion Back-End creada con NestJS, TypeScript, TypeORM y PostgreSQL')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   app.use(LoggerMiddleware);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
