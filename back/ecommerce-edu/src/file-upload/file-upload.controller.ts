f7cc9d078b01af64fd9235d8068e5f9b3e302858
Eduardo Ruiz <ruizzmarin20@gmail.com>
2024-08-06 13:51:17 -0300

    rutas protegidas

--- back/ecommerce-edu/src/file-upload/file-upload.controller.ts
+++ back/ecommerce-edu/src/file-upload/file-upload.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, FileTypeValidator, MaxFileSizeValidator, Param, ParseFilePipe, ParseUUIDPipe, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, FileTypeValidator, MaxFileSizeValidator, Param, ParseFilePipe, ParseUUIDPipe, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FileUploadService } from './file-upload.service';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('files')
 export class FileUploadController {
     constructor(private readonly fileUploadService: FileUploadService){}
 
     @Post('uploadImage/:id')
+    @UseGuards(AuthGuard)
     @UseInterceptors(FileInterceptor('file'))
     async uploadImage(
         @Param('id', ParseUUIDPipe) productId: string,
--- back/ecommerce-edu/src/orders/orders.controller.ts
+++ back/ecommerce-edu/src/orders/orders.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post, UseGuards } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './orders.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @ApiTags('orders')
 @Controller('orders')
@@ -9,12 +10,14 @@ export class OrdersController {
     constructor(private readonly orderService: OrdersService){}
 
     @Post()
+    @UseGuards(AuthGuard)
     addOrder(@Body() order: CreateOrderDto){
         const { userId, products } = order;
         return this.orderService.addOrder(userId, products);
     }
 
     @Get(':id')
+    @UseGuards(AuthGuard)
     getOrder(@Param('id', new ParseUUIDPipe()) id: string) {
         return this.orderService.getOrder(id);
     }
